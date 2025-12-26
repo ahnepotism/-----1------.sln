9365a76e5d0b09029c3a11f3056ebee15b19f7be
ahnepotism <tsvetov09@gmail.com>
2025-12-27 01:15:49 +0300

    Add files via upload

---
+++ CiurseWork.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+#include "Laba1.h"
+#include "Laba2.h"
+#include "Laba3.h"
+#include "Laba4.h"
+int main() {
+	setlocale(LC_ALL, "");
+	int choicee;
+	while (true) {
+		cout << "Введите номер:\n";
+		cin >> choicee;
+		switch (choicee) {
+		case 1:
+			Laba1();
+			break;
+		case 2:
+			Laba2();
+			break;
+		case 3:
+			Laba3();
+			break;
+		case 4:
+			Laba4();
+			break;
+
+		case 0:
+			return 0;
+			break;
+		}
+	}
+
+
+
+}
